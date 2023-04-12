214d886c441c6a486ff29f047645d534a1e00022
Pratik <morepratik600.com>
2023-04-13 00:17:26 +0530

    first commit

---
+++ SpringWebAppMavenMVC/src/main/java/org/tnsif/springwebmvc/SpringWebAppMavenMvcApplication.java
@@ -0,0 +1,12 @@
+package org.tnsif.springwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppMavenMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppMavenMvcApplication.class, args);
+	}
+}
